d22c6deb807b0051862edae6f79cde5e8008db0f
sean6 <sean6@DESKTOP-QRM65VO>
2022-10-25 19:11:38 +0800

    加入專案檔案。

---
+++ Q2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			for (int i = 1; i <= 5; i++)
+			{
+				int zero = 5 - i;
+				int star = i * 2 - 1;
+				string row = new string(' ', zero) + new string('*', star);
+				Console.WriteLine(row);
+			}
+		}
+	}
+}
---
+++ Q3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q3
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			for (int i = 1; i <= 5; i++)
+			{
+				int zero = 5 - i;
+				int star = i;
+				string row = new string(' ', zero) + new string('*', star);
+				Console.WriteLine(row);
+			}
+		}
+	}
+}
